6513b08095ab907a1034d57143d8d67f894ffb96
9kyo-hwang <ryghkd507@naver.com>
2026-08-06 16:41:31 +0900

    [Feature] Scene Graph based on transform
    - TransformComponent에서 부모 계층 표현
    - 부모 계층을 고려한 Get/Set Position 로직 구현

--- CraftEngine/Component/TransformComponent.cpp
+++ CraftEngine/Component/TransformComponent.cpp
@@ -12,12 +12,24 @@ namespace Craft
 
     Vector2 TransformComponent::GetWorldPosition() const
     {
-        return localPosition;   // TEMP
+        if (auto parent = GetParent())
+        {
+            // ex) 3, 0 + 1, 2 -> 4, 2
+            return parent->GetWorldPosition() + localPosition;
+        }
+
+        return localPosition;
     }
 
     void TransformComponent::SetWorldPosition(const Vector2& position)
     {
-        localPosition = position;   // TEMP
+        if (auto parent = GetParent())
+        {
+            localPosition = position - parent->GetWorldPosition();
+            return;
+        }
+
+        localPosition = position;
     }
 
     void TransformComponent::SavePreviousWorldPosition()
--- CraftEngine/Component/TransformComponent.h
+++ CraftEngine/Component/TransformComponent.h
@@ -19,14 +19,18 @@ namespace Craft
         void SetWorldPosition(const Vector2& position);
 
         inline Vector2 GetPreviousWorldPosition() const { return previousWorldPosition; }
-
         void SavePreviousWorldPosition();
+
+        inline std::shared_ptr<TransformComponent> GetParent() const { return parent.lock(); }
+        inline void SetParent(std::weak_ptr<TransformComponent> newParent) { parent = newParent; }
         
     protected:
         // Actor가 들고 있던 위치 정보를 이관
         Vector2 localPosition;  // 부모 기준으로 얼마나 떨어져있는가
         Vector2 worldPosition;  // 월드 좌표 기준으로 어디에 위치하는가(일단은 localPosition으로 다 사용)
         Vector2 previousWorldPosition;
+
+        std::weak_ptr<TransformComponent> parent;   // 부모를 약참조로 가리키자.
     };
 }
 
