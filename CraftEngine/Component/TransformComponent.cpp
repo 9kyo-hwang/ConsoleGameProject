ef73fc16c2ae523008ec80d5c9fec75ccb244c1f
9kyo-hwang <ryghkd507@naver.com>
2026-08-06 11:55:41 +0900

    [Feature] Add transform component

---
+++ CraftEngine/Component/TransformComponent.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "TransformComponent.h"
+
+namespace Craft
+{
+    TransformComponent::TransformComponent(const Vector2& localPosition)
+        : localPosition(localPosition)
+        , previousWorldPosition(localPosition)  // TEMP
+    {
+
+    }
+
+    Vector2 TransformComponent::GetWorldPosition() const
+    {
+        return localPosition;   // TEMP
+    }
+
+    void TransformComponent::SetWorldPosition(const Vector2& position)
+    {
+        localPosition = position;   // TEMP
+    }
+
+    void TransformComponent::SavePreviousWorldPosition()
+    {
+        previousWorldPosition = GetWorldPosition();
+    }
+}
---
+++ CraftEngine/Component/TransformComponent.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <Component/ActorComponent.h>
+#include <Math/Vector2.h>
+
+namespace Craft
+{
+    class TransformComponent : public ActorComponent
+    {
+        TYPE_DECLARATIONS(TransformComponent, ActorComponent)
+
+    public:
+        TransformComponent(const Vector2& localPosition = Vector2::Zero);
+        ~TransformComponent() override = default;
+
+        // TODO: 부모 계층 붙으면 위치 Getter/Setter 로직 수정
+        inline Vector2 GetLocalPosition() const { return localPosition; }
+        inline void SetLocalPosition(const Vector2& position) { localPosition = position; } 
+        Vector2 GetWorldPosition() const;   
+        void SetWorldPosition(const Vector2& position);
+
+        inline Vector2 GetPreviousWorldPosition() const { return previousWorldPosition; }
+
+        void SavePreviousWorldPosition();
+        
+    protected:
+        // Actor가 들고 있던 위치 정보를 이관
+        Vector2 localPosition;  // 부모 기준으로 얼마나 떨어져있는가
+        Vector2 worldPosition;  // 월드 좌표 기준으로 어디에 위치하는가(일단은 localPosition으로 다 사용)
+        Vector2 previousWorldPosition;
+    };
+}
+
