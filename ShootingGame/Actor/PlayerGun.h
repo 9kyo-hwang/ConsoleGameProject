14a3d34241de33cff5b13752a1f9ef42fbab7621
9kyo-hwang <ryghkd507@naver.com>
2026-08-08 23:09:32 +0900

    [Feature] Add PlayerGun, PlayerEngineEffect
    - PlayerGun(발사 위치 제공자), PlayerEngineEffect(엔진 추진 이펙트 표시자) 액터 추가

---
+++ ShootingGame/Actor/PlayerGun.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <Actor/Actor.h>
+
+// Player에 붙어서 총알 발사 위치를 제공하는 액터
+class PlayerGun : public Craft::Actor
+{
+    TYPE_DECLARATIONS(PlayerGun, Craft::Actor)
+
+public:
+    // 총알이 발사될 로컬 위치
+    PlayerGun(const Craft::Vector2& localPosition);
+    ~PlayerGun() override = default;
+
+    // 총알이 생성될 '월드' 좌표 반환
+    Craft::Vector2 GetFirePosition() const;
+};
+
