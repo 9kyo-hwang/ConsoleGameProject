3156ebd388e42dbe5155c989a6f934da96b89df2
9kyo-hwang <ryghkd507@naver.com>
2026-08-10 16:35:41 +0900

    [Test] Z1에 SpriteCell 기반 임시 액터로 테스트 완료
    - Clipping
    - Transparent 옵션

--- Z1/Level/GameplayLevel.cpp
+++ Z1/Level/GameplayLevel.cpp
@@ -3,11 +3,60 @@
 #include <Render/Renderer.h>
 #include <Core/Input.h>
 #include <Game/Game.h>
+#include <Component/SpriteRendererComponent.h>
+#include <Actor/Actor.h>
 
 using namespace Craft;
 
+namespace
+{
+    std::shared_ptr<const Sprite> CreateTestSprite()
+    {
+        std::vector<SpriteCell> cells
+        {
+            {' ', 0, true},
+            {'^', (WORD)Color::Green, false},
+            {' ', 0, true},
+            {' ', 0, true},
+            {' ', 0, true},
+
+            {'<', (WORD)Color::Yellow, false},
+            {'#', (WORD)Color::Red, false},
+            {'>', (WORD)Color::Yellow, false},
+            {' ', 0, true},
+            {' ', 0, true},
+
+            {' ', 0, true},
+            {'v', (WORD)Color::Green, false},
+            {' ', 0, true},
+            {' ', 0, true},
+            {' ', 0, true},
+        };
+
+        return std::make_shared<const Sprite>(
+            Vector2(5, 3),
+            std::move(cells)
+        );
+    }
+}
+
 GameplayLevel::GameplayLevel()
 {
+    _testSprite = CreateTestSprite();
+}
+
+void GameplayLevel::BeginPlay()
+{
+    Level::BeginPlay();
+
+    if (_spriteActor)
+    {
+        return;
+    }
+
+    // Actor의 Render는 컴포넌트에 의해.
+    _spriteActor = SpawnActor<Actor>(Vector2(-1, -1));
+    _spriteActor->AddComponent<SpriteRendererComponent>(_testSprite, 10);
 }
 
 void GameplayLevel::Tick(float deltaTime)
--- Z1/Level/GameplayLevel.h
+++ Z1/Level/GameplayLevel.h
@@ -1,13 +1,24 @@
 ﻿#pragma once
 #include <Level/Level.h>
 
+namespace Craft
+{
+    class Actor;
+    class Sprite;
+}
+
 class GameplayLevel : public Craft::Level
 {
 public:
     GameplayLevel();
     ~GameplayLevel() override = default;
 
+    void BeginPlay() override;
     void Tick(float deltaTime) override;
     void Draw() override;
+
+private:
+    std::shared_ptr<Craft::Actor> _spriteActor;
+    std::shared_ptr<const Craft::Sprite> _testSprite;
 };
 
