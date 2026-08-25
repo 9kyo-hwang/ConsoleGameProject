0f0c794d2f9823301a89ed987d5a2a7dfa52edcb
9kyo-hwang <ryghkd507@naver.com>
2026-08-25 16:58:29 +0900

    [Refactor] Engine에 최소한의 Tilemap 기능 추가
    - Z1의 Map/Level 그리드, 타일맵 구조에서 일반화 가능한 영역만 Engine 기능으로 승격
    - 이후 Z1 리팩토링

---
+++ CraftEngine/Tilemaps/Tilemap.cpp
@@ -0,0 +1,105 @@
+﻿#include "pch.h"
+#include "Tilemap.h"
+#include <cassert>
+
+namespace Craft
+{
+    Tilemap::Tilemap(Vector2 size, Vector2 cellSize)
+        : _size(size), _cellSize(cellSize)
+    {
+        assert(_size.x > 0 && _size.y > 0);
+        assert(_cellSize.x > 0 && _cellSize.y > 0);
+
+        _tiles.resize((size_t)size.x * size.y);
+    }
+
+    bool Tilemap::IsInBounds(Vector2 cell) const
+    {
+        return 0 <= cell.x && cell.x < _size.x && 0 <= cell.y && cell.y < _size.y;
+    }
+
+    const Tile* Tilemap::GetTile(Vector2 cell) const
+    {
+        if (!IsInBounds(cell)) return nullptr;
+        return &_tiles[GetIndex(cell)];
+    }
+
+    bool Tilemap::SetTile(Vector2 cell, Tile tile)
+    {
+        if (!IsInBounds(cell) || !tile.sprite || tile.sprite->GetSize() != _cellSize)
+        {
+            return false;
+        }
+
+        _tiles[GetIndex(cell)] = std::move(tile);
+        return true;
+    }
+
+    bool Tilemap::CanPlaceBox(const Box2D& box) const
+    {
+        if (!box.IsValid())
+        {
+            return false;
+        }
+
+        const Vector2 mapWorldSize = _size * _cellSize;
+        if (box.Left() < 0 || box.Top() < 0 || box.Right() >= mapWorldSize.x || box.Bottom() >= mapWorldSize.y)
+        {
+            return false;
+        }
+
+        const int minTileX = box.Left()     / _cellSize.x;
+        const int minTileY = box.Top()      / _cellSize.y;
+        const int maxTileX = box.Right()    / _cellSize.x;
+        const int maxTileY = box.Bottom()   / _cellSize.y;
+
+        for (int tileY = minTileY; tileY <= maxTileY; ++tileY)
+        {
+            for (int tileX = minTileX; tileX <= maxTileX; ++tileX)
+            {
+                if (GetTile(Vector2(tileX, tileY))->blocked)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    std::shared_ptr<const Sprite> Tilemap::BuildSprite(Vector2 origin, Vector2 count) const
+    {
+        const Vector2 spriteSize = count * _cellSize;
+        std::vector<SpriteCell> cells(spriteSize.x * spriteSize.y);
+
+        for (int tileY = 0; tileY < count.y; ++tileY)
+        {
+            for (int tileX = 0; tileX < count.x; ++tileX)
+            {
+                const Tile* tile = GetTile(origin + Vector2(tileX, tileY));
+                if (!tile || !tile->sprite)
+                {
+                    continue;
+                }
+
+                const Vector2 targetOrigin = Vector2(tileX, tileY) * _cellSize;
+                for (int cellY = 0; cellY < _cellSize.y; ++cellY)
+                {
+                    for (int cellX = 0; cellX < _cellSize.x; ++cellX)
+                    {
+                        const int targetIndex = (targetOrigin.y + cellY) * spriteSize.x + (targetOrigin.x + cellX);
+                        cells[targetIndex] = tile->sprite->GetCell(cellX, cellY);
+                    }
+                }
+            }
+        }
+
+        return std::make_shared<const Sprite>(spriteSize, std::move(cells));
+    }
+
+    size_t Tilemap::GetIndex(Vector2 cell) const
+    {
+        assert(IsInBounds(cell));
+        return (size_t)cell.y * _size.x + cell.x;
+    }
+}
---
+++ CraftEngine/Tilemaps/Tilemap.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <Core/Core.h>
+#include <Render/Sprite.h>
+#include <memory>
+#include <Math/Vector2.h>
+#include <Math/Box2D.h>
+#include <vector>
+
+namespace Craft
+{
+    struct CRAFT_API Tile
+    {
+        std::shared_ptr<const Sprite> sprite = nullptr;
+        bool blocked = false;
+    };
+
+    class CRAFT_API Tilemap
+    {
+    public:
+        Tilemap(Vector2 size, Vector2 cellSize);
+
+        inline Vector2 GetSize() const { return _size; }           // 셀 단위 타일맵 크기
+        inline Vector2 GetCellSize() const { return _cellSize; }   // 각 셀 크기
+
+        bool IsInBounds(Vector2 cell) const;  // 해당 셀이 Tilemap 범위에 들어오는지
+        
+        inline Vector2 CellToWorld(Vector2 cell) const { return cell * _cellSize; }    // 논리 -> 실제 픽셀
+        inline Vector2 WorldToCell(Vector2 world) const { return Vector2(world.x / _cellSize.x, world.y / _cellSize.y); }  // 픽셀 -> 논리
+        inline Box2D GetCellBounds(Vector2 cell) const { return Box2D(CellToWorld(cell), _cellSize); } // 픽셀 기준 좌표, 픽셀 기준 크기
+
+        const Tile* GetTile(Vector2 cell) const;
+        bool SetTile(Vector2 cell, Tile tile);
+
+        bool CanPlaceBox(const Box2D& box) const;   // TilemapCollider2D 대신...
+        std::shared_ptr<const Sprite> BuildSprite(Vector2 origin, Vector2 count) const; // origin 위치에서 count개의 타일로 구성된 Sprite
+
+    private:
+        size_t GetIndex(Vector2 cell) const;
+
+    private:
+        Vector2 _size;      // 타일맵 크기(타일 개수 단위)
+        Vector2 _cellSize;  // 논리 타일 하나의 월드/콘솔 크기. Z1에선 10 x 5
+        std::vector<Tile> _tiles;
+    };
+}
+
+
